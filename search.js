22adf79207351c828ef115539eb325d407957d43
SajayBailey <sajaymarsh@gmail.com>
2016-11-10 16:21:55 -0600

    Exercise 3 complete

--- search.js
+++ search.js
@@ -3,20 +3,39 @@ window.onload = function(){
   button.addEventListener("click",search);
   
   var sch = new XMLHttpRequest();
+  var result = document.getElementById("result")
+  
+  var myUrl = "https://info2180-lab6-sajaybailey.c9users.io/request.php/request.php?q=";
+  //url = url + searchVal;
   
   
   function search(){
-    sch.open("GET","https://info2180-lab6-sajaybailey.c9users.io/request.php/request.php?q=definition", true);
-    sch.send();
+    var searchVal = document.getElementById("lookup").value;
     
-    sch.onreadystatechange = function(){
-      if (sch.readyState===4 && sch.status===200){
-        var response = sch.responseText;
-        alert(response);
-      }else{
-        alert("Checking...");
+    //Check if text entered in textfield
+    if(searchVal.length === 0){
+      result.innerHTML = "Empty search!";
+      
+    }else{
+      
+      var url = myUrl + searchVal;
+      
+      sch.open("GET",url, true);
+      sch.send();
+      
+      sch.onreadystatechange = function(){
+        if (sch.readyState===4 && sch.status===200){
+          var response = sch.responseText;
+          result.innerHTML = response;
+          
+		}else{
+          result.innerHTML = "Ivalid search!";
+         
+        }
       }
     }
+    
+    
     
     //alert("test!");
   }
