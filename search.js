5f50b442d75c2fb484172f3321aa45fc3586a9db
SajayBailey <sajaymarsh@gmail.com>
2016-11-11 22:58:15 -0600

    Exercise 4 complete

--- search.js
+++ search.js
@@ -2,13 +2,16 @@ window.onload = function(){
   var button = document.getElementById("button");
   button.addEventListener("click",search);
   
+  var allDef = document.getElementById("all");
+  allDef.addEventListener("click",allSearch);
+  
   var sch = new XMLHttpRequest();
   var result = document.getElementById("result")
   
   var myUrl = "https://info2180-lab6-sajaybailey.c9users.io/request.php/request.php?q=";
-  //url = url + searchVal;
   
   
+  //search when search button clicked.
   function search(){
     var searchVal = document.getElementById("lookup").value;
     
@@ -17,7 +20,7 @@ window.onload = function(){
       result.innerHTML = "Empty search!";
       
     }else{
-      
+      //alert("else");
       var url = myUrl + searchVal;
       
       sch.open("GET",url, true);
@@ -28,16 +31,69 @@ window.onload = function(){
           var response = sch.responseText;
           result.innerHTML = response;
           
-		}else{
+        }else{
           result.innerHTML = "Ivalid search!";
-         
+          
         }
       }
     }
-    
-    
-    
-    //alert("test!");
+  }
+  
+  //Search for all definitions.
+  function allSearch(){
+    result.innerHTML = "";
+    sch.open("GET","https://info2180-lab6-sajaybailey.c9users.io/request.php/request.php?q=&all=true", true);
+      sch.send();
+      
+      sch.onreadystatechange = function(){
+        if (sch.readyState===4 && sch.status===200){
+    			var xmlRes = sch.responseXML;
+    			//alert(xmlRes);
+    			
+    			
+    			//get definition (returns array of elements)
+    			var def = xmlRes.getElementsByTagName("definition");
+    			
+    			//create ordered ist and list item
+    			var list = document.createElement("ol");
+    			
+    			
+    			
+    			//iterate through definition tags and add to list.
+    			for (var i =0; i<def.length; i++){
+    			  
+    			  //create tags
+    			  var heading = document.createElement("h1");
+    			  var desc = document.createElement("p");
+    			  var auth = document.createElement("p");
+    			  var item = document.createElement("li");
+    			  
+    			  //create texts for tags
+    			  var text = document.createTextNode(def[i].childNodes[0].nodeValue);
+    			  var h = document.createTextNode(def[i].attributes[0].value);
+    			  var a = document.createTextNode('-'+def[i].attributes[1].value);
+    			  
+    			  ///add texts to tags
+    			  heading.appendChild(h);
+    			  desc.appendChild(text);
+    			  auth.appendChild(a);
+    			  
+    			  //append tags to list item.
+    			  item.appendChild(heading);
+    			  item.appendChild(desc);
+    			  item.appendChild(auth);
+    			  
+    			  list.appendChild(item);
+    			  
+    			}
+    			
+    			result.appendChild(list);
+            
+            
+        }else{ 
+          //Bleh.
+        }
+      }
   }
   
 }
