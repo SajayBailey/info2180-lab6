34493a57c12879742f30cdd3d159f61aa2bbb0fb
SajayBailey <sajaymarsh@gmail.com>
2016-11-11 23:13:05 -0600

    Exercise 4 complete

--- request.php
+++ request.php
@@ -1,8 +1,7 @@
 <?php
-
+/*
 // accept a term (keyword)
 // respond with a value
-
 $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
@@ -13,6 +12,82 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+*/
+
+
+header ("Content-Type:text/xml");
+$xmlInfo=<<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+
+<entries>
+    
+        <definition name ="definition" author ="Kane">
+        A statement of the exact meaning of a word, especially in a dictionary.
+        </definition>
+        
+        
+        
+            <definition name ="bar" author ="Bobby">
+        A place that sells alcholic beverages.</definition>
+        
+        
+        
+            <definition name ="ajax" author ="DR. Who">
+        Technique which involves the use of javascript and xml (or JSON).
+        </definition>
+        
+        
+        
+            <definition name ="html" author ="Someone">
+          The standard markup language for creating web pages and web applications.
+        </definition>
+        
+        
+        
+        <definition name ="css" author ="Hakon">
+            A style sheet language used for describing the presentation of a document written in a markup language.
+        </definition>
+        
+        
+        
+        <definition name ="javascript" author ="Brendan">
+            A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+        </definition>
+        
+        
+        
+        <definition name ="php" author ="Rasmus">
+            A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+        </definition>
+        
+</entries>
+XML;
+
+//$xmlOut=new SimpleXMLElement($xmlInfo);
+print $xmlInfo;
+
+/*
+foreach($xmlOut->children() as $child){
+    print $child->attributes();
+    print $child;
+    //print $child[0]->attributes() . "<br>";
+    //foreach($xmlOut->definition[0]->attributes() as $a => $b){
+   //     echo $b,"<br>";
+   // }
+   // echo $child . "<br>";
+}
+*/
+
+
+//echo $xmlOut->children();
+/*
+echo $xmlOut->getName() . "<br>";
+
+foreach ($xmlOut->children() as $child)
+  {
+  echo $child->getName() . "<br>";
+  }*/
+
+?>
